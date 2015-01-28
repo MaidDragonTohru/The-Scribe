2476a8f34ed953a9f337e2ccf94b16d8ff75bb9e
Alexander Lazenby-Catherwood <axebane007@hotmail.com>
2015-01-28 13:53:33 +0800

    Short patch to ;time and ;wotd
    Fixed issue with hours.

--- commands.js
+++ commands.js
@@ -872,7 +872,7 @@ exports.commands = {
         //And one more, just for good luck.
         if (dd === 32) { this.ddd = "thirty-second" };
         var AMorPM = "AM"
-        if (hr = 12) AMorPM = "PM"
+        if (hr === 12) AMorPM = "PM"
         if (hr === 24) { hr = 12; AMorPm = "AM" };
         if (hr > 12) {
             if (hr === 13) { hr = 1 };
@@ -972,7 +972,7 @@ exports.commands = {
         //And one more, just for good luck.
         if (dd === 32) { this.ddd = "thirty-second" };
         var AMorPM = "AM"
-        if (hr = 12) AMorPM = "PM"
+        if (hr === 12) AMorPM = "PM"
         if (hr === 24) { hr = 12; AMorPm = "AM" };
         if (hr > 12) {
             if (hr === 13) { hr = 1 };
