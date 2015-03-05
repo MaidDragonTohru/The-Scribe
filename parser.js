37106eb722428301d977299b7bfdfd812ac452b5
Quinton Lee <quivistis@gmail.com>
2015-03-05 12:49:52 -0600

    Use /blockchallenges upon logging in
    /idle was updated to also block PMs, so /blockchallenges must be used instead

--- parser.js
+++ parser.js
@@ -158,7 +158,7 @@ exports.parse = {
 				ok('logged in as ' + spl[2]);
 
 				// Now join the rooms
-				var cmds = ['|/idle'];
+				var cmds = ['|/blockchallenges'];
 				for (var i in config.rooms) {
 					var room = toId(config.rooms[i]);
 					if (room === 'lobby' && config.serverid === 'showdown') {
